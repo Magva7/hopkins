0122dbbb8359568ee174ced3c0dc4b40572c503a
Magva7 <magva7@gmail.com>
2021-03-03 00:26:14 +0300

    Сделал шаг 8 и поправил функцию из шага 4 - убрал у нее имя

--- myhopkins/js/SpeakGoodBye.js
+++ myhopkins/js/SpeakGoodBye.js
@@ -17,7 +17,7 @@ var byeSpeaker = {};
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  function speak(name) {
+  byeSpeaker.sayWord = function (name) {
     console.log(speakWord + " " + name);
   }
 
--- myhopkins/js/SpeakHello.js
+++ myhopkins/js/SpeakHello.js
@@ -15,7 +15,7 @@ var helloSpeaker = {};
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  helloSpeaker.sayWord = function speak(name) {
+  helloSpeaker.sayWord = function (name) {
     console.log(speakWord + " " + name);
   }
 
