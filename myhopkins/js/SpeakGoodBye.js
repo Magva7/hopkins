fea0bb45e061ec32c73ac19d04b5fdee9a33453b
Magva7 <magva7@gmail.com>
2021-03-03 00:27:24 +0300

    Запилил шаг 9

--- myhopkins/js/SpeakGoodBye.js
+++ myhopkins/js/SpeakGoodBye.js
@@ -23,5 +23,5 @@ var byeSpeaker = {};
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 })();
